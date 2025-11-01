f3564e679f51b61b476400539740524e717607a0
Nandhana Sasikumar <121442002+Nandhu2004@users.noreply.github.com>
2025-11-01 22:25:49 +0530

    delete

--- src/main/java/com/example/demo/repository/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-import com.example.demo.entity.Token;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface TokenRepository extends CrudRepository<Token, Long> {
-    Optional<Token> findByToken(String token);
-}
